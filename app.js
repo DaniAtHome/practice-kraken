be4c5cb48b13525e46a45eb6a08efa52671d15e6
DaniAtHome <danielle_arteaga@hotmail.com>
2016-05-09 22:32:04 -0700

    add jquery animation

---
+++ app.js
@@ -0,0 +1,16 @@
+//--------------------------------------------
+//	DOCUMENT READY
+//---------------------------------------------
+
+$(document).ready(function() {
+	$('.js-like').on('click', function(event) {
+		event.preventDefault();
+		
+		$(this).text('Liked')
+		.addClass('is-liked');
+	})
+});
+
+// give a href class of .js-like'
+
+
